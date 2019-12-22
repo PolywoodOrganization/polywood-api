2eb2e070a6c6d8c6606687ec09855a1d25fe3c5b
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2019-12-22 10:36:55 +0100

    fix login

--- src/main/java/com/polywood/api/controller/AuthController.java
+++ src/main/java/com/polywood/api/controller/AuthController.java
@@ -5,12 +5,14 @@ import com.google.common.hash.Hashing;
 import com.polywood.api.auth.Authenticator;
 import com.polywood.api.exceptions.InternalServerErrorException;
 import com.polywood.api.exceptions.UnauthorizedException;
+import com.polywood.api.model.LoginForm;
 import com.polywood.api.model.UsersEntity;
 import com.polywood.api.respositories.UsersEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
 import java.nio.charset.StandardCharsets;
 
@@ -25,15 +27,16 @@ public class AuthController {
     }
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(method = RequestMethod.GET, value = "/login", produces = "application/json")
-    public ResponseEntity<String> login(@RequestParam String login, @RequestParam String password) throws RuntimeException {
-        UsersEntity user = usersRepository.findByLogin(login);
+    @RequestMapping(method = RequestMethod.POST, value = "/login", produces = "application/json")
+    public ResponseEntity<String> login(@RequestBody LoginForm loginForm) throws RuntimeException {
+
+        UsersEntity user = usersRepository.findByLogin(loginForm.getLogin());
 
         if (user == null) {
             throw new UnauthorizedException(); //HTTP 401
         }
 
-        String receivedHash = Hashing.sha256().hashString(password, StandardCharsets.UTF_8).toString();
+        String receivedHash = Hashing.sha256().hashString(loginForm.getPassword(), StandardCharsets.UTF_8).toString();
 
         if (!receivedHash.equals(user.getPassword())) {
             throw new UnauthorizedException(); //HTTP 401
---
+++ src/main/java/com/polywood/api/model/LoginForm.java
@@ -0,0 +1,14 @@
+package com.polywood.api.model;
+
+public class LoginForm {
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
