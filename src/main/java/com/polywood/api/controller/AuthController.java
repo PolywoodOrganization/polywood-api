30c8d00bbac3ab7bff2a49266809d16a4fedc2b7
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2020-01-12 16:46:20 +0100

    login function return login + connected user

--- src/main/java/com/polywood/api/controller/AuthController.java
+++ src/main/java/com/polywood/api/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.polywood.api.controller;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.google.common.hash.Hashing;
 import com.polywood.api.auth.Authenticator;
+import com.polywood.api.dto.LoginResponseDto;
 import com.polywood.api.exceptions.InternalServerErrorException;
 import com.polywood.api.exceptions.UnauthorizedException;
 import com.polywood.api.model.LoginForm;
@@ -28,22 +29,26 @@ public class AuthController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST, value = "/login", produces = "application/json")
-    public ResponseEntity<String> login(@RequestBody LoginForm loginForm) throws RuntimeException {
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginForm loginForm) throws RuntimeException {
 
         UsersEntity user = usersRepository.findByLogin(loginForm.getLogin());
 
         if (user == null) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); //HTTP 401
+            throw new UnauthorizedException(); //HTTP 401
         }
 
         String receivedHash = Hashing.sha256().hashString(loginForm.getPassword(), StandardCharsets.UTF_8).toString();
 
         if (!receivedHash.equals(user.getPassword())) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); //HTTP 401
+            throw new UnauthorizedException(); //HTTP 401
         }
 
+        LoginResponseDto response = new LoginResponseDto();
+        response.setUser(user);
+        response.setToken(Authenticator.buildToken(user));
+
         try {
-            return new ResponseEntity<>(Authenticator.buildToken(user), HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
 
         } catch (JWTCreationException exception) {
             System.out.println(exception.getMessage());
